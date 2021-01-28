ed56b45218cbe607c1b44d85f4a002b099774dbb
daniela vela <danielitavo97@gmail.com>
2021-01-28 04:20:20 -0400

    fix webpack server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-ReactDOM.render(<h1>Hello word</h1>, document.getElementById('root'))
+ReactDOM.render(<h1>hola mundo</h1>, document.getElementById('root'))
