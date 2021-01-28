45e1a9434c01b728ecebdcee120235ba680d847f
daniela vela <danielitavo97@gmail.com>
2021-01-28 05:20:37 -0400

    install bootswatch

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import ReactDOM from 'react-dom'
 
 import List from './containers/List'
 
+import 'bootswatch/dist/lux/bootstrap.min.css'
+
 ReactDOM.render(<List/>, document.getElementById('root'))
